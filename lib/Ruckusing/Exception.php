2836a57026f57f6dfee58e2875f33f32fda54d1f
Salimane Adjao Moustapha <me@salimane.com>
2013-01-13 20:10:08 +0800

    make Ruckusing_Exception compatible with php 5.2

--- lib/Ruckusing/Exception.php
+++ lib/Ruckusing/Exception.php
@@ -34,7 +34,6 @@ class Ruckusing_Exception extends Exception
     const INVALID_MIGRATION_DIR = 110;
     const INVALID_FRAMEWORK = 111;
     const QUERY_ERROR = 112;
-    const INVALID_MIGRATION_METHOD = 113;
 
     /**
      * Redefine the exception so message isn't optional
@@ -48,7 +47,11 @@ class Ruckusing_Exception extends Exception
     public function __construct($message, $code = 0, Exception $previous = null)
     {
         // make sure everything is assigned properly
-        parent::__construct($message, $code, $previous);
+        if (version_compare(PHP_VERSION, '5.3.0', '>=')) {
+            parent::__construct($message, $code, $previous);
+        } else {
+            parent::__construct($message, $code);
+        }
     }
 
     /**
